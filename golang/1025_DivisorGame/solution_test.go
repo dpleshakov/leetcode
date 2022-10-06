76123e4a3152d59c989c6e6b0543f327341ebdf7
Pleshakov Dmitry <dpleshakov@gmail.com>
2022-10-06 08:09:21 +0300

    Minor refactoring

--- golang/1025_DivisorGame/solution_test.go
+++ golang/1025_DivisorGame/solution_test.go
@@ -4,22 +4,22 @@ import (
 	"testing"
 )
 
-func TestDefault1(t *testing.T) {
+func TestDivisorGame1(t *testing.T) {
 	n := 2
 	want := true
 
 	res := divisorGame(n)
 	if res != want {
-		t.Fatalf(`divisorGame(%q) = %t, want match for %t`, n, res, want)
+		t.Fatalf("divisorGame(%q) = %t, want match for %t", n, res, want)
 	}
 }
 
-func TestDefault2(t *testing.T) {
+func TestDivisorGame2(t *testing.T) {
 	n := 3
 	want := false
 
 	res := divisorGame(n)
 	if res != want {
-		t.Fatalf(`divisorGame(%q) = %t, want match for %t`, n, res, want)
+		t.Fatalf("divisorGame(%q) = %t, want match for %t", n, res, want)
 	}
 }
