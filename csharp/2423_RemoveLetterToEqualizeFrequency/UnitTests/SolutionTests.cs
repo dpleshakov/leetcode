9a58a30e25acac48309110a25fbd6fcc3d9c3a7a
Pleshakov Dmitry <dpleshakov@gmail.com>
2023-02-06 08:05:27 +0300

    Initial unit tests

---
+++ csharp/2423_RemoveLetterToEqualizeFrequency/UnitTests/SolutionTests.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using _2423_RemoveLetterToEqualizeFrequency;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2423_RemoveLetterToEqualizeFrequency.Tests
+{
+    [TestClass()]
+    public class SolutionTests
+    {
+        [TestMethod()]
+        public void EqualFrequencyTest() {
+            Assert.Fail();
+        }
+    }
+}
