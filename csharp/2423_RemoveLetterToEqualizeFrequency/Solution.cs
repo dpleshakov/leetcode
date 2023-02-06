2dbd6144b5fec0914e3dcc250007b36608ed0537
Pleshakov Dmitry <dpleshakov@gmail.com>
2023-02-06 07:58:03 +0300

    Initial 2423_RemoveLetterToEqualizeFrequency

---
+++ csharp/2423_RemoveLetterToEqualizeFrequency/Solution.cs
@@ -0,0 +1,9 @@
+﻿namespace _2423_RemoveLetterToEqualizeFrequency
+{
+    public class Solution
+    {
+        public bool EqualFrequency(string word) {
+            throw new NotImplementedException();
+        }
+    }
+}
