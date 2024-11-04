cf12e0ddc241697c1639d8dc7a16a577595ccc8d
Dmitry Pleshakov <dpleshakov@gmail.com>
2024-11-04 12:24:01 +0300

    Initialize 36_ValidSudoku

---
+++ cpp/36_ValidSudoku/src/solution.h
@@ -0,0 +1,13 @@
+#ifndef _36_VALIDSUDOKU_H_
+#define _36_VALIDSUDOKU_H_
+
+#include <vector>
+
+class Solution {
+ public:
+  bool isValidSudoku(const std::vector<std::vector<char>> &board) {
+    return false;
+  }
+};
+
+#endif  // _36_VALIDSUDOKU_H_
