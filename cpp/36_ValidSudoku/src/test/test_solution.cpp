789fd1c62340f6a74b21cfa796bda47a03adb312
Dmitry Pleshakov <dpleshakov@gmail.com>
2024-11-17 09:43:07 +0300

    Add tests

--- cpp/36_ValidSudoku/src/test/test_solution.cpp
+++ cpp/36_ValidSudoku/src/test/test_solution.cpp
@@ -57,6 +57,33 @@ TEST_F(SolutionTest, DISABLED_isValidSudokuTest) {
   EXPECT_FALSE(solution_.isValidSudoku(invalid_board_));
 }
 
+TEST_F(SolutionTest, IsRowValidTest) {
+  for (size_t row_index = 0; row_index < valid_board_.size(); ++row_index) {
+    EXPECT_TRUE(solution_.Test_IsRowValid(valid_board_, row_index)) << "Row index: " << row_index;
+  }
+
+  for (size_t row_index = 0; row_index < invalid_board_.size(); ++row_index) {
+    EXPECT_TRUE(solution_.Test_IsRowValid(invalid_board_, row_index)) << "Row index: " << row_index;
+  }
+}
+
+TEST_F(SolutionTest, IsColumnValidTest) {
+  ASSERT_TRUE(valid_board_.size() > 0);
+
+  for (size_t column_index = 0; column_index < valid_board_[0].size(); ++column_index) {
+    EXPECT_TRUE(solution_.Test_IsColumnValid(valid_board_, column_index)) << "Column index: " << column_index;
+  }
+
+  ASSERT_TRUE(invalid_board_.size() > 0);
+  ASSERT_TRUE(invalid_board_[0].size() > 0);
+
+  EXPECT_FALSE(solution_.Test_IsColumnValid(invalid_board_, 0)) << "Column index: 0";
+
+  for (size_t column_index = 1; column_index < invalid_board_[0].size(); ++column_index) {
+    EXPECT_TRUE(solution_.Test_IsColumnValid(invalid_board_, column_index)) << "Column index: " << column_index;
+  }
+}
+
 TEST_F(SolutionTest, IsDigitsValidTest) {
   for (const auto& digits : valid_digits_) {
     EXPECT_TRUE(solution_.Test_IsDigitsValid(digits)) << "Digits: " << ::testing::PrintToString(digits);
